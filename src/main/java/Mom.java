f8adb0064dd84b22be182c1fb1bd0f3eaeef8729
botosek <seansim02@gmail.com>
2025-01-21 15:26:37 +0800

    Implement initial skeletal code

---
+++ src/main/java/Mom.java
@@ -0,0 +1,12 @@
+public class Mom {
+    public static void main(String[] args) {
+        String chatbotName = "Mom";
+        String divider = "--------------------------------------------------";
+
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+
+        System.out.println("Hope to see you again!");
+    }
+}
