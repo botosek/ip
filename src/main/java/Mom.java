da9ff2ec6caea550293ef4023aa2d731d905a112
botosek <seansim02@gmail.com>
2025-01-22 11:31:46 +0800

    Add ability to mark and unmark tasks as done and undone respectively

--- src/main/java/Mom.java
+++ src/main/java/Mom.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Mom {
     static String chatbotName = "Mom";
     static String divider = "--------------------------------------------------";
-    static String[] taskList = new String[100];
+    static Task[] taskList = new Task[100];
     static int  taskIndex = 0;
 
     public static void main(String[] args) {
@@ -17,21 +17,54 @@ public class Mom {
 
         do {
             input = scan.nextLine();
+            String[] inputList = input.split(" ");
 
             if (input.equals("list")) {
                 System.out.println(divider);
                 for(int i = 0; i < taskIndex; i++) {
                     int listIndex = i + 1;
-                    System.out.println(listIndex + ". " + taskList[i]);
+                    System.out.println(listIndex + ". "
+                            + taskList[i].getStatusIcon() + " "
+                            + taskList[i].getDescription());
                 }
                 System.out.println(divider + "\n");
+
+            } else if (inputList[0].equals("mark")) {
+                for(int i = 0; i < taskIndex; i++) {
+                    if (taskList[i].getDescription().equals(input.substring(5))) {
+                        taskList[i].mark();
+
+                        System.out.println(divider);
+                        System.out.println("Nice! I've marked this task as done.");
+                        System.out.println("    " + taskList[i].getStatusIcon()
+                                + " " + taskList[i].getDescription());
+                        System.out.println(divider + "\n");
+                        break;
+                    }
+                }
+
+            } else if (inputList[0].equals("unmark")) {
+                for(int i = 0; i < taskIndex; i++) {
+                    if (taskList[i].getDescription().equals(input.substring(7))) {
+                        taskList[i].unmark();
+
+                        System.out.println(divider);
+                        System.out.println("Okay, I've unmarked this task as incomplete.");
+                        System.out.println("    " + taskList[i].getStatusIcon()
+                                + " " + taskList[i].getDescription());
+                        System.out.println(divider + "\n");
+                        break;
+                    }
+                }
+
             } else {
-                taskList[taskIndex] = input;
+                taskList[taskIndex] = new Task(input);
                 taskIndex++;
 
                 System.out.println(divider);
                 System.out.println("Task added: " + input);
                 System.out.println(divider + "\n");
+
             }
         } while (!input.equals("bye"));
 
